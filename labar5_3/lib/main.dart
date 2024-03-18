36d86560c3efa768fe1f554228d84e5878354900
Anatoliy Pedan <tolik.pedan@mail.ru>
2024-03-18 22:09:45 +0300

    Готовая лаба с бесконечным списком возведения двойку в степень строки

--- labar5_3/lib/main.dart
+++ labar5_3/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+void kvadrat(index){
+  pow(index, 2);
+}
+
 void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +21,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Список элементов'),
           backgroundColor: Colors.green,
+        ),
+        body: ListView.builder(
+            itemBuilder: (context, index) {
+              if (index.isOdd) {
+                return Divider();}
+              return Text('2 ^ $index = ${pow(2, index)}');
+            }
         ),
       ),
     );
