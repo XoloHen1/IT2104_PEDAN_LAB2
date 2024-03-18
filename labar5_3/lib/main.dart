b7e369f39d073fa7c39456c7138a572bc995f260
Anatoliy Pedan <tolik.pedan@mail.ru>
2024-03-18 21:42:47 +0300

    Добавлена библиотека math

--- labar5_3/lib/main.dart
+++ labar5_3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -6,7 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
